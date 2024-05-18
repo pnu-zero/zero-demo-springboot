257e930c5384a041d082c8f104f97366abf411ca
hunsy9 <juniper0917@gmail.com>
2024-05-18 14:58:05 +0900

    feat: GroupService 추가

---
+++ src/main/java/com/example/zero/group/service/GroupService.java
@@ -0,0 +1,49 @@
+package com.example.zero.group.service;
+
+import com.example.zero.group.domain.model.GroupDto;
+import com.example.zero.group.domain.model.GroupResponseDto;
+import com.example.zero.group.domain.repository.GroupRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class GroupService {
+    private final GroupRepository groupRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public List<GroupResponseDto> getAllGroups() {
+        return groupRepository.findAllGroups();
+    }
+
+    public List<GroupResponseDto> getAllGroupsByUserId(Long userId) {
+        return groupRepository.findGroupsByUserId(userId);
+    }
+
+    @Transactional
+    public GroupDto createGroup(GroupDto groupDto) {
+        if(!groupDto.getPassword().isEmpty()){
+            String encodedPassword = passwordEncoder.encode(groupDto.getPassword());
+            groupDto.setPassword(encodedPassword);
+        }
+
+        groupRepository.createGroup(groupDto);
+        return groupDto;
+    }
+
+    @Transactional
+    public GroupDto updateGroup(GroupDto groupDto) {
+        return null;
+    }
+
+    @Transactional
+    public void mapGroupToUser(Long userId, Long groupId) {
+        groupRepository.createGroupUserRelation(userId, groupId);
+    }
+}
