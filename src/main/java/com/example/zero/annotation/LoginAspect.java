aa9978d712cd303cf768c863060cc5a85b58906b
hunsy9 <juniper0917@gmail.com>
2024-05-18 14:52:05 +0900

    feat: 유저의 Login 여부를 확인하는 어노테이션 추가

---
+++ src/main/java/com/example/zero/annotation/LoginAspect.java
@@ -0,0 +1,29 @@
+package com.example.zero.annotation;
+
+import com.example.zero.utils.SessionUtils;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import jakarta.servlet.http.HttpSession;
+import org.aspectj.lang.JoinPoint;
+
+
+@Aspect
+@Component
+public class LoginAspect {
+
+    @Before("@annotation(com.example.zero.annotation.LoginRequired)")
+    public void memberLoginCheck(JoinPoint jp) throws Throwable {
+
+        HttpSession session = ((ServletRequestAttributes)(RequestContextHolder.currentRequestAttributes())).getRequest().getSession();
+        Long userId = SessionUtils.getLoginUserId(session);
+
+        if (userId == null) {
+            throw new HttpStatusCodeException(HttpStatus.UNAUTHORIZED, "NO_LOGIN") {};
+        }
+    }
+}
---
+++ src/main/java/com/example/zero/user/exception/LoginException.java
@@ -0,0 +1,7 @@
+package com.example.zero.user.exception;
+
+public class LoginException extends RuntimeException{
+    public LoginException(String message) {
+        super(message);
+    }
+}
