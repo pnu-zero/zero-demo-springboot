5d3a90cc7ee4c8c014635429e4839b0deb0b9e3f
hunsy9 <juniper0917@gmail.com>
2024-05-18 14:55:27 +0900

    feat: 인증 메일 발송용 MailService 추가

---
+++ src/main/java/com/example/zero/mail/service/MailService.java
@@ -0,0 +1,48 @@
+package com.example.zero.mail.service;
+
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+    private final JavaMailSender javaMailSender;
+    private final TemplateEngine htmlTemplateEngine;
+
+    private String generateActivationLink(String verificationCode, Long groupId, Long userId) throws UnknownHostException {
+        String hostAddr = InetAddress.getLocalHost().getHostAddress();
+        return "http://localhost:8080/api/user/activate?auth_code=" + verificationCode + "&user_id=" + userId + "&group_id=" + groupId;
+    }
+
+    public String sendAuthMail(String clientEmail, String verificationCode, Long groupId, Long userId) throws Exception {
+        Context context = new Context();
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, "UTF-8");
+
+        InternetAddress from = new InternetAddress("juniper0917@gmail.com", "PNU Cloud");
+        InternetAddress to = new InternetAddress(clientEmail);
+
+        String activationLink = generateActivationLink(verificationCode, groupId, userId);
+
+        context.setVariable("text", activationLink);
+
+        String htmlTemplate = htmlTemplateEngine.process("mail/authMail", context);
+        messageHelper.setFrom(from);
+        messageHelper.setTo(to);
+        messageHelper.setSubject("[PNU Cloud] 사용자 인증 요청 메일");
+        messageHelper.setText(htmlTemplate, true);
+        javaMailSender.send(mimeMessage);
+
+        return null;
+    }
+}
